c42b68b1f8bf211df08512bcabcad8a9c315786e
Srinivas Akella <akellasrinivas9944@gmail.com>
2025-01-31 22:16:18 +0530

    company logo added

--- middleware/verify.js
+++ middleware/verify.js
@@ -45,7 +45,7 @@ const verifyJobOwner = (req, res, next) => {
 			}
 			// console.log(job.userId, req.user.userId);
 			if (job?.userId.equals(req.user.userId)) {
-				console.log(job.userId, req.user.userId);
+				console.log("userIds", job.userId, req.user.userId);
 				next();
 			} else {
 				return res.status(400).json({
