735987e00436598e80ff5dd24df12a1562dcbea6
Srinivas Akella <akellasrinivas9944@gmail.com>
2025-02-09 11:28:09 +0530

    dev

--- helpers/sendJobAppliedMail.js
+++ helpers/sendJobAppliedMail.js
@@ -4,31 +4,38 @@ async function sendJobAppliedMailToEmployer(employerEmail, employerName, user) {
 	try {
 		// Create a transporter using your email service credentials
 		const transporter = nodemailer.createTransport({
-			service: "Gmail", // service provider
+			service: "Gmail",
 			auth: {
 				user: "srinivas72075@gmail.com",
 				pass: "ifhp vypf rhqb ubpw",
 			},
 		});
 
-		// Email content
+		// Email content with clickable resume link
 		const mailOptions = {
 			from: "srinivas72075@gmail.com",
 			to: employerEmail,
 			subject: "Job Applied 📞",
-			html: `Alert ${employerName.split(" ")[0]}, ${
-				user.name.split(" ")[0]
-			} has applied for the job you have posted in JobbyApp. Please check the <a href="${
-				user?.resume
-			}" target="_blank">Resume Here</a>
-			 => USER DETAILS: Name:${user?.name}<br />Email:${user.email}<br />Phone:${
-				user.phone
-			}`,
+			html: `
+				<p>Alert ${employerName.split(" ")[0]},</p>
+				<p>${
+					user.name.split(" ")[0]
+				} has applied for the job you posted on JobbyApp.</p>
+				<p><strong>User Details:</strong></p>
+				<ul>
+					<li><strong>Name:</strong> ${user?.name}</li>
+					<li><strong>Email:</strong> ${user?.email}</li>
+					<li><strong>Phone:</strong> ${user?.phone}</li>
+				</ul>
+				<p>You can <a href="${
+					user?.resume
+				}" target="_blank" style="color:blue; font-weight:bold;">VIEW THE RESUME HERE</a>.</p>
+			`,
 		};
 
 		// Send the email
 		const info = await transporter.sendMail(mailOptions);
-		// console.log("Email sent:", info.messageId);
+		console.log("Email sent:", info.messageId);
 	} catch (error) {
 		console.error("Error sending email:", error);
 	}
